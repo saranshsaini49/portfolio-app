65ee09d508603b09ff4caebea625cd8e9bc500d9
SaranshSAINI <saranshsss99@gmail.com>
2024-01-19 23:22:47 +0530

    frontend setup, header started

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import HeaderContainer from './Header/HeaderContainer';
+
+function App() {
+  return (
+    <>
+      <HeaderContainer/>
+      <div className="bg-red-100">
+        Hello world
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
